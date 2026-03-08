d3ae27048f2a560efd6cfe1375505f9c73e30622
toysun <52024437+toysun@users.noreply.github.com>
2026-03-08 11:39:31 +0900

    Add files via upload

---
+++ ear-tracking/app.js
@@ -0,0 +1,8 @@
+import {earringChainComponent} from './components/earring-chain.js'
+AFRAME.registerComponent('earring-chain', earringChainComponent)
+
+import {uiManagerComponent} from './components/ui-manager.js'
+AFRAME.registerComponent('ui-manager', uiManagerComponent)
+
+import {hideEarringsComponent} from './components/hide-earrings.js'
+AFRAME.registerComponent('hide-earrings', hideEarringsComponent)
