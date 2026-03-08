5b7edb2110728904b4faa1f3111fd7143b624990
toysun <52024437+toysun@users.noreply.github.com>
2026-03-08 12:06:34 +0900

    Add files via upload

---
+++ video-on-mesh/app.js
@@ -0,0 +1,3 @@
+import {videoTextureComponent, videoTextureCameraRollComponent} from './js/video-texture-components.js'
+AFRAME.registerComponent('video-texture', videoTextureComponent)
+AFRAME.registerComponent('video-texture-camera-roll', videoTextureCameraRollComponent)
