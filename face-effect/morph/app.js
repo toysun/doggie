c4fa43e5f0b8525a85e3e60eb9c2f2ef62982b31
toysun <52024437+toysun@users.noreply.github.com>
2026-03-08 11:48:23 +0900

    Add files via upload

---
+++ face-effect/morph/app.js
@@ -0,0 +1,8 @@
+import {gltfMorphComponent} from './components/gltf-morph.js'
+AFRAME.registerComponent('gltf-morph', gltfMorphComponent)
+
+import {animateFaceComponent} from './components/animate-face.js'
+AFRAME.registerComponent('animate-face', animateFaceComponent)
+
+import {uiManagerComponent} from './components/ui-manager.js'
+AFRAME.registerComponent('ui-manager', uiManagerComponent)
