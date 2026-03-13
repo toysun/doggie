d6ba3b89449640393b46bd18145584afb5cfacd2
toysun <52024437+toysun@users.noreply.github.com>
2026-03-14 08:24:26 +0900

    Update sky-recenter.js

--- sky/sky-recenter.js
+++ sky/sky-recenter.js
@@ -8,3 +8,20 @@ AFRAME.registerComponent('sky-recenter', {
     this.el.addEventListener('sky-coaching-overlay.hide', recenter)
   },
 })
+
+AFRAME.registerComponent('hide-show', {
+  init() {
+    const scene = this.el.sceneEl
+    const model = document.getElementById('model')
+
+    // ✅ 하늘 인식 성공 → 리센터 + 모델 표시
+    scene.addEventListener('sky-coaching-overlay.hide', () => {
+      model.setAttribute('visible', true)
+    })
+
+    // ✅ 하늘 이탈 → 모델 숨김
+    scene.addEventListener('sky-coaching-overlay.show', () => {
+      model.setAttribute('visible', false)
+    })
+  },
+})
