4d11f8783a468b41009154940844247b73ea41fc
toysun <52024437+toysun@users.noreply.github.com>
2026-04-24 22:29:42 +0900

    Create app1.js

---
+++ absolute-scale/app1.js
@@ -0,0 +1,20 @@
+import {postprocessingComponent} from './postprocessing.js'
+AFRAME.registerComponent('postprocessing', postprocessingComponent)
+import {changeColorComponent, absPinchScaleComponent, annotationComponent, proximityComponent, gltfMorphComponent, ignoreRaycast} from './js/components.js'
+//AFRAME.registerComponent('change-color', changeColorComponent)
+AFRAME.registerComponent('annotation', annotationComponent)
+AFRAME.registerComponent('absolute-pinch-scale', absPinchScaleComponent)
+AFRAME.registerComponent('proximity', proximityComponent)
+AFRAME.registerComponent('gltf-morph', gltfMorphComponent)
+AFRAME.registerComponent('ignore-raycast', ignoreRaycast)
+
+import {responsiveImmersiveComponent} from './js/responsive-immersive.js'
+AFRAME.registerComponent('responsive-immersive', responsiveImmersiveComponent)
+
+// bitmaps cause texture issues on iOS this workaround prevents black textures and crashes
+const IS_IOS =
+  /^(iPad|iPhone|iPod)/.test(window.navigator.platform) ||
+  (/^Mac/.test(window.navigator.platform) && window.navigator.maxTouchPoints > 1)
+if (IS_IOS) {
+  window.createImageBitmap = undefined
+}
